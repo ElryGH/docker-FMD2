38f5d5c10e545e6dda7999b89c67e368697f4b41
Elry <gh@elry.moe>
2024-10-09 18:02:50 +0200

    Remove bad parts of startup

--- root/custom-cont-init.d/init.sh
+++ root/custom-cont-init.d/init.sh
@@ -5,7 +5,3 @@ git clone --single-branch --depth=1 https://github.com/dazedcat19/FMD2.git /app/
 cp /app/FMD2/src/lua /app/FMD2 -R
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix -R
-chown abc:abc /app -R
-chown abc:abc /config -R
-chown abc:abc /downloads -R
-chmod +x /usr/local/bin/sync_dir
