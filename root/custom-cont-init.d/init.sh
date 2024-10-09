953d3e55a7c886f4ff61f00f520261768a5b6c27
Elry <gh@elry.moe>
2024-10-09 18:26:42 +0200

    Update init.sh

--- root/custom-cont-init.d/init.sh
+++ root/custom-cont-init.d/init.sh
@@ -4,8 +4,8 @@ mkdir -p /app/FMD2/src
 git clone --single-branch --depth=1 https://github.com/dazedcat19/FMD2.git /app/FMD2/src
 cp /app/FMD2/src/lua /app/FMD2 -R
 mkdir -p /tmp/.X11-unix
-chmod 1777 /tmp/.X11-unix -R
-chown abc:abc /app -R
-chown abc:abc /config -R
-chown abc:abc /downloads -R
-RUN chmod +x /usr/local/bin/sync_dir
+sudo chmod 1777 /tmp/.X11-unix -R
+sudo chown abc:abc /app -R
+sudo chown abc:abc /config -R
+sudo chown abc:abc /downloads -R
+sudo chmod +x /usr/local/bin/sync_dir
